dbc52c6d0e944e8962b582f5c69bd3376b60fed7
Abhishek K <abhishekakade@gmail.com>
2019-08-12 02:17:38 +0530

    Updates

---
+++ precache-manifest.5d0923d1e4119da549b11370fca0daec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c689b894f0151d0519281cc63ab05715",
+    "url": "/flickk-app/index.html"
+  },
+  {
+    "revision": "5da530dd918c8e812fa0",
+    "url": "/flickk-app/static/css/main.0e8aa222.chunk.css"
+  },
+  {
+    "revision": "f654ac6a6f84a41b13cd",
+    "url": "/flickk-app/static/js/2.d9d8b21b.chunk.js"
+  },
+  {
+    "revision": "5da530dd918c8e812fa0",
+    "url": "/flickk-app/static/js/main.ce78c6e5.chunk.js"
+  },
+  {
+    "revision": "190d3c5b755ec517a239",
+    "url": "/flickk-app/static/js/runtime~main.805df534.js"
+  },
+  {
+    "revision": "c35f1447c758ce24196d18bde969d80a",
+    "url": "/flickk-app/static/media/404.c35f1447.jpg"
+  },
+  {
+    "revision": "02a9430b88975cae16fcfcc9cf7b5799",
+    "url": "/flickk-app/static/media/tmdb.02a9430b.svg"
+  }
+]);
